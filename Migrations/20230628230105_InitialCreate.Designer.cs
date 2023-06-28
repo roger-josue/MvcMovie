d1b4fcaff262b2e48d9bf094f0b4d140559fc3d2
roger-josue <jovz1798@gmail.com>
2023-06-28 17:05:02 -0600

    Added new field and ran db migration

--- Migrations/20230628230105_InitialCreate.Designer.cs
+++ Migrations/20230628230105_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20230628190157_InitialCreate")]
+    [Migration("20230628230105_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
